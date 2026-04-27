3a238693599e9aef4f4bc0da5f30f641e11d5dd6
Edgard Bouillot <edgard.bouillot@mailo.com>
2026-04-27 21:25:50 +0200

    [FIX:.\eval\dataset.py] RUNNING, fixed the way data was loaded

--- eval/dataset.py
+++ eval/dataset.py
@@ -66,8 +66,8 @@ class VOC12(Dataset):
 class cityscapes(Dataset):
     def __init__(self, root, input_transform=None, target_transform=None, subset='val'):
         root = str(Path(__file__).resolve().parents[1])
-        self.images_root = os.path.join(root,"ValidationDatasets", 'leftImg8bit' + subset)
-        self.labels_root = os.path.join(root,"ValidationDatasets", 'gtFine' + subset)
+        self.images_root = os.path.join(root,"ValidationDatasets", "cityscapes", 'leftImg8bit' + subset)
+        self.labels_root = os.path.join(root,"ValidationDatasets", "cityscapes", 'gtFine' + subset)
 
         self.filenames = [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser(self.images_root)) for f in fn if is_image(f)]
         self.filenames.sort()
